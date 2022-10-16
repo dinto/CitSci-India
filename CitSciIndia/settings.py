7ef255c00daa2e3f190359ecf1b6f31437ece772
dinto <dinto.t.d@gmail.com>
2022-10-16 15:36:03 +0530

    Update settings.py

--- CitSciIndia/settings.py
+++ CitSciIndia/settings.py
@@ -127,8 +127,8 @@ STATICFILES_DIRS=[
 os.path.join(BASE_DIR,'static')
 ]
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-django_heroku.settings(locals())
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#django_heroku.settings(locals())
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
