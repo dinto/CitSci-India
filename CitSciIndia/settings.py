3e8771bd9abf5b2f63b480985823991976b1cceb
dinto <dinto.t.d@gmail.com>
2022-10-16 16:25:02 +0530

    Update settings.py

--- CitSciIndia/settings.py
+++ CitSciIndia/settings.py
@@ -134,3 +134,4 @@ os.path.join(BASE_DIR,'static')
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+WebServer_Port=8080
